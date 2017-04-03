5fc6d873490d1d3a00a3c21b69ba1fbf09c7be91
caramirezal <cramireza@ciencias.unam.mx>
2017-04-02 20:03:54 -0500

    Testing SQUAD main file

--- SQUAD.R
+++ SQUAD.R
@@ -3,7 +3,7 @@
 # boolNetToSQUAD project
 # July 6, 2016. Ciudad de Mexico.
 # Written by Carlos Ramirez Alvarez.
-# Automatically translate a Boolean network model from boolNet R package format to 
+# Automatically translates a Boolean network model from boolNet R package format to 
 # a continuous network model using the SQUAD method as
 # described in Martinez-Sosa, 2013. Biosystems. Vol. 113.
 # Also add new functions for boolean regulatory network modeling
@@ -17,9 +17,19 @@ library(BoolNet)
 library(reshape)
 
 # new functions
+source("https://raw.githubusercontent.com/caramirezal/SQUAD/master/loadNetworkSQUAD.R")
 source("https://raw.githubusercontent.com/caramirezal/SQUAD/master/asContinuous.R")
-source("https://raw.githubusercontent.com/caramirezal/SQUAD/master/plotSequence.R")
-source("https://github.com/caramirezal/SQUAD/blob/master/loadNetworkSQUAD.R")
+source("https://raw.githubusercontent.com/caramirezal/SQUAD/master/getAttractorsAsynchronous.R")
+source("https://raw.githubusercontent.com/caramirezal/SQUAD/master/squad.R")
+
+
+if ( ! ( "GINsim-2.9.4-with-deps.jar" %in% list.files() ) ) {
+        download.file("http://ginsim.org/sites/default/files/ginsim-dev/GINsim-v2.9.4.tgz")
+}
+
+if ( ! ( "GetAttractors.java" %in% list.files() ) ) {
+        download.file("https://raw.githubusercontent.com/caramirezal/SQUAD/master/GetAttractors.java")
+}
 
 #source("graphToModel.R")
 #source("plotSequence.R")

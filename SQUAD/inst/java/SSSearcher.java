bab5c5fb26ad1509e00b61bdd625ac0e180b9437
caramirezal <cramireza@ciencias.unam.mx>
2017-08-15 23:45:20 -0500

    Removing labels

--- SQUAD/inst/java/SSSearcher.java
+++ SQUAD/inst/java/SSSearcher.java
@@ -7,67 +7,61 @@ import org.ginsim.gui.graph.dynamicgraph.StableTableModel;
 import org.ginsim.service.format.sbml.SBMLqualService;
 import org.ginsim.service.tool.stablestates.StableStatesService;
 
- 
+
 
 public class SSSearcher {
 		public static int[][] getAttractors(){
-			
+
 			// Importing sbml file to ginsim
 			RegulatoryGraph Net;
 			SBMLqualService S = new SBMLqualService();
 			String pathFile = "regulatoryNetworkModel.sbml";
 			Net = S.importLRG(pathFile);
-			
-			
 
-			// Getting the stable states searcher service 
+			// Getting the stable states searcher service
 			org.colomoto.logicalmodel.tool.stablestate.StableStateSearcher sss;
 			StableStatesService sser = new StableStatesService();
 			sss = sser.getSearcher(Net);
-			
+
 			// Running the method
 			sss.run();
-			System.out.println(sss.getStatus());
-			//StableTableModel statStates;
-			//statStates = new StableTableModel(Net);			
-			//statStates.setResult(sss.getMDDManager(), sss.getResult() );
-			
+			//System.out.println(sss.getStatus());
+
 			// setting the results in the model object
 			StableTableModel model;
-			model = new StableTableModel(Net);			
+			model = new StableTableModel(Net);
 			model.setResult(sss.getMDDManager(), sss.getResult() );
-			
+
 			int nrow = model.getRowCount();
-			System.out.println("nrow "+ nrow);
-			
+			//System.out.println("nrow "+ nrow);
+
 			int res[][] = new int[nrow][];
-			
+
 			// This needs to be modified
 			if ( nrow > 0 ) {
-			        
+
 			        int ncol = model.getState(0).length;
-			        System.out.println("ncol "+ ncol);
-			
+			        //System.out.println("ncol "+ ncol);
+
 			        res = new int[nrow][ncol];
-			
+
 			        for (int i=0;i<nrow;i++){
 				        for (int j=0;j<ncol;j++){
 					        res[i][j] = model.getState(i)[j];
 				        }
 			        }
-			        
+
 			}
-			
+
 			if ( nrow <= 0 ) {
-			        
+
 			        res = new int[0][0];
-			        
-			        System.out.println("No fixed points found! Maybe all solutions are cycles");
-			        
+			        //System.out.println("No fixed points found! Maybe all solutions are cycles");
+
 			}
-			
+
 			return res;
-			
+
 		}
 
 }
